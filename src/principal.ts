faa2b9e123effedcba76b118b78d8478dce0d866
juanpi-241005 <juanpi.241005@gmail.com>
2025-09-28 15:34:33 -0400

    notas en principal

--- src/principal.ts
+++ src/principal.ts
@@ -4,9 +4,11 @@ import Cl_controlador from "./Cl_controlador.js";
 
 export default class Cl_principal {
     constructor(){
+      // Inicialización de la aplicación
       let vista = new Cl_vFundacion();
       let modelo = new Cl_mFundacion();
       let controlador = new Cl_controlador(modelo,vista);
+      // Asignación del controlador a la vista
       vista.controlador = controlador;
     }
 }
