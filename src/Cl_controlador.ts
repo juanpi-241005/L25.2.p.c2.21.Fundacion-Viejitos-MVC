82f1545139f3782c5acdc9b833ae4765db3d97ff
juanpi-241005 <juanpi.241005@gmail.com>
2025-09-28 15:21:15 -0400

    notas en Cl_controlador

--- src/Cl_controlador.ts
+++ src/Cl_controlador.ts
@@ -5,14 +5,16 @@ export default class Cl_controlador {
     public modelo: Cl_mFundacion;
     public vista: Cl_vFundacion;
 
-    constructor(modelo: Cl_mFundacion, vista: Cl_vFundacion){
+    constructor(modelo: Cl_mFundacion, vista: Cl_vFundacion){ // Inyección de dependencias
         this.modelo = modelo;
         this.vista = vista;
     }
 
     procesarDonante(){
-        this.modelo.procesarDonante(this.vista.procesarDonante());
+        // Procesamiento del donante y actualización del reporte en la vista
+        this.modelo.procesarDonante(this.vista.procesarDonante()); // Llamada al método de la vista para obtener el donante
         this.vista.reporteDonante({
+            // Parámetros con valores por defecto
             totalFinalDolares: this.modelo.totalFinalDolares(), 
             totalFinalBolivares: this.modelo.totalFinalBolivares(), 
             mayorDonante: this.modelo.mayorDonante()
