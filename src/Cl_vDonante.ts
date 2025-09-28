1d73d14e8bc501a41a249a22293ac3e6727c380c
juanpi-241005 <juanpi.241005@gmail.com>
2025-09-28 13:20:53 -0400

    Notas en Cl_vDonante

--- src/Cl_vDonante.ts
+++ src/Cl_vDonante.ts
@@ -8,14 +8,17 @@ export default class Cl_vDonante {
     public btProcesar: HTMLElement;
 
     constructor(){
+        // Inicialización de los elementos del DOM (inputs y botón)
         this.inNombre = document.getElementById("donanteForm_inNombre") as HTMLInputElement;
         this.inDolares = document.getElementById("donanteForm_inDolares") as HTMLInputElement;
         this.inBolivares = document.getElementById("donanteForm_inBolivares") as HTMLInputElement;
         this.btProcesar = document.getElementById("donanteForm_btProcesar") as HTMLElement;
 
+        // Verificación de que los elementos existen en el DOM
         if(!this.inNombre || !this.inDolares || !this.inBolivares || !this.btProcesar)
             throw new Error("Error en la inicialización de la vista Donante");
 
+        // Asignación del evento click al botón de procesar
         this.btProcesar.onclick = () => {
             if(!this.controlador)
                 throw new Error("Error: No hay controlador en la vista Donante");
@@ -23,6 +26,7 @@ export default class Cl_vDonante {
         };
     }
 
+    // Getters para obtener los valores de los campos del formulario
     get nombre(): string {
         if(!this.inNombre) 
             throw new Error("Error: No hay campo nombre en la vista Donante");
